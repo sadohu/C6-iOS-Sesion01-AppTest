071ef762e15c64f15f2110ae31bb7bdf617c28e9
sadohu <sadohu26@yahoo.com>
2023-10-09 18:17:16 -0500

    Add: Design y atributtes

---
+++ AppTables/AppTables/ItemCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCollectionViewCell.swift
+//  AppTables
+//
+//  Created by Sadohu on 9/10/23.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imgLibro: UIImageView!
+    @IBOutlet weak var lblLibro: UILabel!
+    
+}
---
+++ AppTables/AppTables/LibroViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibroViewController.swift
+//  AppTables
+//
+//  Created by Sadohu on 6/10/23.
+//
+
+import UIKit
+
+class LibroViewController: UIViewController {
+    @IBOutlet weak var cvLibros: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
