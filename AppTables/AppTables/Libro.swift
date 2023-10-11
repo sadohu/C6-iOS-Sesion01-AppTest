c326206f146a875161f1da664c15f19f168ad37c
sadohu <sadohu26@yahoo.com>
2023-10-11 14:05:14 -0500

    Add: Struct Libro

---
+++ AppTables/AppTables/Libro.swift
@@ -0,0 +1,16 @@
+//
+//  Libro.swift
+//  AppTables
+//
+//  Created by Sadohu on 9/10/23.
+//
+
+import UIKit
+
+struct Libro {
+    var codigo : Int
+    var titulo : String
+    var precio : Double
+    var cantidad : Int
+    var foto : String
+}
