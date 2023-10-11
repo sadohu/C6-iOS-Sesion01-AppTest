036313683bbae0e7c669ac608395cc40d9df591e
sadohu <sadohu26@yahoo.com>
2023-10-11 14:11:40 -0500

    Add: Filling libros with assets

--- AppTables/AppTables/LibroViewController.swift
+++ AppTables/AppTables/LibroViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class LibroViewController: UIViewController , UICollectionViewDataSource{
+    
+    var libroList : [Libro] = [];
+    
     @IBOutlet weak var cvLibros: UICollectionView!
     
     override func viewDidLoad() {
@@ -20,5 +23,15 @@ class LibroViewController: UIViewController , UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         <#code#>
+    }
+    
+    func fillDataLibroList(){
+        libroList.append(Libro(codigo: 1, titulo: "Libro 1", precio: 25.6, cantidad: 30, foto: "harry1"));
+        libroList.append(Libro(codigo: 1, titulo: "Libro 2", precio: 15.6, cantidad: 10, foto: "harry2"));
+        libroList.append(Libro(codigo: 1, titulo: "Libro 3", precio: 35.6, cantidad: 20, foto: "harry3"));
+        libroList.append(Libro(codigo: 1, titulo: "Libro 4", precio: 25.6, cantidad: 40, foto: "harry4"));
+        libroList.append(Libro(codigo: 1, titulo: "Libro 5", precio: 15.6, cantidad: 10, foto: "harry5"));
+        libroList.append(Libro(codigo: 1, titulo: "Libro 6", precio: 35.6, cantidad: 20, foto: "harry6"));
+        libroList.append(Libro(codigo: 1, titulo: "Libro 7", precio: 25.6, cantidad: 30, foto: "harry7"));
     }
 }
