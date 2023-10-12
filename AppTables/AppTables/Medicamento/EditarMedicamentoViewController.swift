94069ff80f185a6b64989c781d0f64ec92ff00f2
sadohu <sadohu26@yahoo.com>
2023-10-11 22:10:10 -0500

    Add: EditarMedicamento StoryBoard

---
+++ AppTables/AppTables/Medicamento/EditarMedicamentoViewController.swift
@@ -0,0 +1,27 @@
+//
+//  EditarMedicamentoViewController.swift
+//  AppTables
+//
+//  Created by Sadohu on 11/10/23.
+//
+
+import UIKit
+
+class EditarMedicamentoViewController: UIViewController {
+
+    @IBOutlet weak var txtCodigo: UITextField!
+    @IBOutlet weak var txtNombre: UITextField!
+    @IBOutlet weak var txtDescripcion: UITextView!
+    @IBOutlet weak var txtStock: UITextField!
+    @IBOutlet weak var txtPrecio: UITextField!
+    @IBOutlet weak var txtFecha: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+
+    @IBAction func btnEditar(_ sender: UIButton) {
+        
+    }
+    
+}
