78664a407c4b93dd7af847404e87bad0e5f177f7
sadohu <sadohu26@yahoo.com>
2023-10-18 22:42:03 -0500

    Add: CreateNewUser and save Data

--- AppTables/AppTables/Authentication/NewUserViewController.swift
+++ AppTables/AppTables/Authentication/NewUserViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewUserViewController: UIViewController {
     @IBOutlet weak var txtNombre: UITextField!
@@ -16,10 +18,56 @@ class NewUserViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func btnRegistrar(_ sender: UIButton) {
+        let name = txtNombre.text ?? "";
+        let lastName = txtApellido.text ?? "";
+        let age = Int(txtEdad.text ?? "0") ?? 0;
+        let userName = txtUsuario.text ?? "";
+        let password = txtContrasena.text ?? "";
+        
+        addAuthenticacion(name: name, lastName: lastName, age: age, userName: userName, password: password);
+        
+    }
+    
+    func addAuthenticacion(name : String, lastName : String, age : Int, userName : String, password : String){
+        // Grabar autenticacion
+        Auth.auth().createUser(withEmail: userName, password: password){result, error in
+            if let dataAuth = result{
+                // Capturar UID de la Autenticacion
+                let uid = dataAuth.user.uid;
+                self.addUser(name: name, lastName: lastName, age: age, uid: uid);
+            }else{
+                self.alertView(message: "Error en el registro de credenciales.")
+            }
+        }
+    }
+    
+    func addUser(name : String, lastName : String, age : Int, uid : String){
+        /* Grabar usuario en Firestore */
+        // Obtener acceso a la base de datos
+        let database = Firestore.firestore();
+        // Acceder a la coleccion usuarios
+        database.collection("usuarios").document(uid).setData([
+            "nombre": name,
+            "apellido": lastName,
+            "edad": age
+        ]){error in
+            // Validar posibles errores
+            if let existError = error{
+                self.alertView(message: "Existe error en registro de usuario y credenciales.");
+            }else{
+                self.alertView(message: "Registro de usuario y credenciales correctas.");
+            }
+        }
+    }
+    
+    func alertView(message : String){
+        let alert = UIAlertController(title: "Sistema", message: message, preferredStyle: .alert);
+        alert.addAction(UIAlertAction(title: "Aceptar", style: .default));
+        present(alert, animated: true);
     }
 }
