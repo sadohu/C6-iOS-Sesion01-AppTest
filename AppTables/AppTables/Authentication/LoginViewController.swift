2de8d0def2f062f529cb7098fe326051182ca5be
sadohu <sadohu26@yahoo.com>
2023-10-18 20:33:20 -0500

    Add: Config for firebase

---
+++ AppTables/AppTables/Authentication/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  AppTables
+//
+//  Created by Sadohu on 18/10/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var txtUser: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+    @IBAction func btnLogin(_ sender: UIButton) {
+        
+    }
+    
+
+}
